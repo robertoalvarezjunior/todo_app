479b6946b3b04e3995b8bde5597446c4ca80565f
Roberto Carlos <roberto.alvarez@robertoalvarezjunior.com>
2023-01-03 17:05:13 -0300

    .

--- lib/database/db.dart
+++ lib/database/db.dart
@@ -1,1 +1,33 @@
+import 'dart:io';
 
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DB {
+  DB._();
+
+  static final DB instance = DB._();
+  static Database? _database;
+  static List<String> q = [];
+
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+
+    String path = join(documentDirectory.path, 'lista_compras.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {},
+    );
+  }
+
+  Future<void> criarBanco(Database db) async {
+    for (String query in q) {
+      await db.execute(query);
+    }
+  }
+}
--- lib/models/department.dart
+++ lib/models/department.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class Department extends ChangeNotifier {
@@ -11,4 +13,25 @@ class Department extends ChangeNotifier {
   });
 
   List<Department> departmentList = [];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'color': color?.value,
+    };
+  }
+
+  factory Department.fromMap(Map<String, dynamic> map) {
+    return Department(
+      id: map['id'] != null ? map['id'] as String : null,
+      title: map['title'] != null ? map['title'] as String : null,
+      color: map['color'] != null ? Color(map['color'] as int) : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Department.fromJson(String source) =>
+      Department.fromMap(json.decode(source) as Map<String, dynamic>);
 }
